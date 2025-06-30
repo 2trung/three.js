2ffac74cef03c554742de4c6c38aef55319dc6f6
2trung <trung.nguyen@msn.com>
2025-06-30 22:50:40 +0700

    Create firework scene with shaders

---
+++ 34-fireworks-shaders/src/shaders/firework/vertex.glsl
@@ -0,0 +1,50 @@
+uniform float uSize;
+uniform vec2 uResolution;
+attribute float aSize;
+uniform float uProgress;
+attribute float aTimeMultiplier;
+
+#include '../includes/remap.glsl'
+
+void main() {
+  float progress = uProgress * aTimeMultiplier;
+  vec3 newPosition = position;
+
+  // Exploding
+  float explodingProgress = remap(progress, 0.0, 0.1, 0.0, 1.0);
+  explodingProgress = clamp(explodingProgress, 0.0, 1.0);
+  explodingProgress = 1.0 - pow(1.0 - explodingProgress, 2.0);
+  newPosition *= explodingProgress;
+
+  // Falling
+  float fallingProgress = remap(progress, 0.1, 1.0, 0.0, 1.0);
+  fallingProgress = clamp(fallingProgress, 0.0, 1.0);
+  fallingProgress = 1.0 - pow(1.0 - fallingProgress, 2.0);
+  newPosition.y -= fallingProgress * 0.2;
+
+  // Scaling
+  float sizeOpenProgress = remap(progress, 0.0, 0.125, 0.0, 1.0);
+  float sizeCloseProgress = remap(progress, 0.125, 1.0, 1.0, 0.0);
+  float sizeProgress = min(sizeOpenProgress, sizeCloseProgress);
+  sizeProgress = clamp(sizeProgress, 0.0, 1.0);
+
+  // Twinkling
+  float twinklingProgress = remap(progress, 0.2, 0.8, 0.0, 1.0);
+  twinklingProgress = clamp(twinklingProgress, 0.0, 1.0);
+  float sizeTwinkling = sin(progress * 30.0) * 0.5 + 0.5;
+  sizeTwinkling = 1.0 - sizeTwinkling * sizeTwinkling; 
+   
+
+
+  vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+
+  gl_Position = projectionMatrix * viewPosition;
+
+  gl_PointSize = uSize * uResolution.y * aSize * sizeProgress * sizeTwinkling;
+  gl_PointSize *= 1.0 / - viewPosition.z;
+
+  if (gl_PointSize < 1.0) {
+    gl_Position = vec4(9999.9);
+  }
+}
